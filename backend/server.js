a58d8037309530e879fa4ca74a1c3861618d3a64
projetbddgit <mailprojetbdd@gmail.com>
2025-12-17 18:30:04 +0100

    test

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ const upload = multer({
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 // ---------------------------
